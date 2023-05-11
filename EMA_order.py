2c6c3686fe1441b9289825d7d3d40243852fc058
Chen Feng Tsai <rich.tsai1103@gmail.com>
2023-05-10 19:35:35 -0500

    ena

--- EMA_order.py
+++ EMA_order.py
@@ -1,9 +1,4 @@
 import pandas as pd
-import time
-import alpaca_trade_api as api
-import configparser
-import random
-
 
 # Define a function to calculate EMA values
 def calculate_ema(data, window):
@@ -11,36 +6,35 @@ def calculate_ema(data, window):
 
 # Define a function to generate trading signals
 def generate_signals(data, short_window, long_window):
+    signals = pd.DataFrame(index=data.index)
+    signals['signal'] = 0
 
     # Calculate EMA values for short and long windows
     ema_short = calculate_ema(data, short_window)
     ema_long = calculate_ema(data, long_window)
 
-    # # Generate trading signals based on EMA crossovers
-    # signals.loc[short_window:, 'signal'] = \
-    #     (ema_short[short_window:] > ema_long[short_window:]).astype(int)
+    # Generate trading signals based on EMA crossovers
+    signals.loc[short_window:, 'signal'] = \
+        (ema_short[short_window:] > ema_long[short_window:]).astype(int)
 
-    # # Calculate the difference between consecutive signals to find the signal changes
-    # signals['positions'] = signals['signal'].diff()
-    
-    
+    # Calculate the difference between consecutive signals to find the signal changes
+    signals['positions'] = signals['signal'].diff()
 
     return signals
 
+# Example usage
+# Load historical price data into a DataFrame
+# Here, 'data' should contain the 'Date' and 'Close' columns
+data = pd.read_csv('historical_data.csv')
+data['Date'] = pd.to_datetime(data['Date'])
+data.set_index('Date', inplace=True)
 
+# Define the short and long EMA windows
 short_window = 20
 long_window = 50
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-api_key = config.get('alpaca', 'API_KEY')
-api_secret = config.get('alpaca', 'SECRET_KEY')
-BASE_URL = "https://paper-api.alpaca.markets"
+# Generate trading signals
+signals = generate_signals(data['Close'], short_window, long_window)
 
-alpaca = api.REST(api_key, api_secret, BASE_URL)
-while True:
-    data = pd.read_csv('btcusd_bar_data.csv')
-    data['timestamp'] = pd.to_datetime(data['timestamp'])
-    data.set_index('timestamp', inplace=True)
-    generate_signals(data['close'], short_window, long_window)
-    time.sleep(1)
+# Print the signals DataFrame
+print(signals)
